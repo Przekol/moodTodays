ced9aa1d94da4fba9975acd77182d34e57cf3ec6
Przemyslaw <przemyslaw.r.wojciechowski@gmail.com>
2022-04-13 11:50:16 +0200

    add class Popup

---
+++ js/Popup.js
@@ -0,0 +1,39 @@
+
+import { UI } from './UI.js';
+import { Form } from './Form.js';
+
+export class Popup extends UI {
+	constructor(userName) {
+		super();
+		this.userName = userName;
+		this.popup = this.getElement(this.UiSelectors.popup);
+		this.wrapper = this.getElement(this.UiSelectors.wrapper);
+	}
+	init() {
+		this.showPopup();
+	
+		this.addListenerToForm()
+
+		// this.popup.addEventListener('click', () => {
+		// 	this.closePopup();
+		// });
+	}
+
+	checkNameUser() {}
+
+	showPopup() {
+		this.popup.classList.add('show');
+		this.body.classList.add('hidden');
+	}
+
+	closePopup() {
+		this.popup.classList.add('fade-out');
+		this.popup.classList.remove('show');
+		this.body.classList.remove('hidden');
+		this.wrapper.classList.add('fade-in');
+	}
+	addListenerToForm() {
+		const formPopup = new Form()
+		formPopup.init()
+	}
+}
