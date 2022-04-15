cce95cd0445006b5f90db33c7e14eca45496aa66
Przemyslaw <przemyslaw.r.wojciechowski@gmail.com>
2022-04-15 16:25:57 +0200

    add function "moodInit"

--- js/Popup.js
+++ js/Popup.js
@@ -1,25 +1,33 @@
-import { UI } from './UI.js';
+import {UI} from './UI.js';
+import {Mood} from './Mood.js';
 
 export class Popup extends UI {
-	constructor() {
-		super();
-		this.popup = this.getElement(this.UiSelectors.popup);
-		this.wrapper = this.getElement(this.UiSelectors.wrapper);
-	}
-	init() {
-		this.showPopup();
-	}
+    constructor() {
+        super();
+        this.popup = this.getElement(this.UiSelectors.popup);
+        this.wrapper = this.getElement(this.UiSelectors.wrapper);
+    }
 
-	showPopup() {
-		this.popup.classList.add('show');
-		this.body.classList.add('hidden');
-		this.wrapper.classList.add('has-fade');
-	}
+    init() {
+        this.showPopup();
+    }
 
-	closePopup() {
-		this.popup.classList.add('fade-out');
-		this.popup.classList.remove('show');
-		this.body.classList.remove('hidden');
-		this.wrapper.classList.add('fade-in');
-	}
+    showPopup() {
+        this.popup.classList.add('show');
+        this.body.classList.add('hidden');
+        this.wrapper.classList.add('has-fade');
+    }
+
+    closePopup() {
+        this.popup.classList.add('fade-out');
+        this.popup.classList.remove('show');
+        this.body.classList.remove('hidden');
+        this.wrapper.classList.add('fade-in');
+        this.moodInit();
+    }
+
+    moodInit() {
+        const mood = new Mood();
+        mood.init();
+    }
 }
