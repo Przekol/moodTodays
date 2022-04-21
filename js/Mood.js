c122f785600962c7ec87bd13f4da52d3ff2c9651
Przemyslaw <przemyslaw.r.wojciechowski@gmail.com>
2022-04-21 14:26:47 +0200

    correct variables month

--- js/Mood.js
+++ js/Mood.js
@@ -95,7 +95,8 @@ export class Mood extends UI {
 	getCurrentDate = () => {
 		const date = new Date();
 		const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
-		const month = date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
+		const month =
+			date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
 		const year = date.getFullYear();
 		return `${day}/${month}/${year}`;
 	};
