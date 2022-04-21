169458cd6904432601004006dd33717ea01f1682
Przemyslaw <przemyslaw.r.wojciechowski@gmail.com>
2022-04-21 09:33:56 +0200

    finish work StatisticsMood.js

--- js/StatisticsMood.js
+++ js/StatisticsMood.js
@@ -1,4 +1,5 @@
 import { UI } from './UI.js';
+import { Advice } from './Advice.js';
 
 export class StatisticsMood extends UI {
 	constructor() {
@@ -9,34 +10,115 @@ export class StatisticsMood extends UI {
 		this.lastDayStatistics = [
 			...this.getElements(this.UiSelectors.lastDayStatistics),
 		];
-		this.last7DayStatistics = [
-			...this.getElements(this.UiSelectors.last7DayStatistics),
+		this.allDaysStatistics = [
+			...this.getElements(this.UiSelectors.allDaysStatistics),
 		];
-		this.btnAdvice = this.getElement(this.UiSelectors.btnAdvice);
+		this.dataStatistics = [...this.getElements(this.UiSelectors.statistics)];
+		this.advice = this.addAdvice();
 	}
 	init() {
-		this.showStatistics();
-
-		this.btnAdvice.addEventListener('click', e => {
-			console.log(e);
-		});
+		this.showStatisticsDays();
+		this.advice.init();
 	}
-	showStatistics() {
+	addAdvice() {
+		return new Advice();
+	}
+	showStatisticsDays() {
 		const moodArray = this.loadFromLocalStorage('mood');
 		this.showTodayStatistics(moodArray);
 	}
 	showTodayStatistics(moodArray) {
-		for (const today of this.todayStatistics) {
-			const mood = today.dataset.mood;
-			if (mood === 'bad') {
-				today.textContent = moodArray[0].emotions.bad;
+		const sumMood = this.getSumMoodStatisticsAllDays(moodArray);
+
+		for (const data of this.dataStatistics) {
+			const mood = data.dataset.mood;
+			const day = data.dataset.day;
+			const text = data.dataset.subtitle;
+
+			switch (day) {
+				case 'today':
+					this.showMoodStatisticsDay(mood, data, moodArray, 0);
+					break;
+				case 'last-day':
+					if (!(moodArray.length < 2)) {
+						this.showTitleStatistics(text, data, moodArray);
+						this.showMoodStatisticsDay(mood, data, moodArray, 1);
+					}
+					break;
+				case 'all-days':
+					this.showTitleStatistics(text, data, moodArray);
+					this.showMoodStatisticsAllDays(mood, data, sumMood);
+					break;
+				default:
+					break;
 			}
-			if (mood === 'bored') {
-				today.textContent = moodArray[0].emotions.bored;
-			}
-			if (mood === 'happy') {
-				today.textContent = moodArray[0].emotions.happy;
-			}
+		}
+	}
+
+	showTitleStatistics(text, data, moodArray) {
+		switch (text) {
+			case 'last-day':
+				data.textContent = ` ${moodArray[1].date}`;
+				break;
+			case 'all-days':
+				data.textContent = ` ${moodArray.length}: od ${
+					moodArray[moodArray.length - 1].date
+				} do ${moodArray[0].date}`;
+				break;
+			default:
+				break;
+		}
+	}
+
+	showMoodStatisticsDay(mood, data, moodArray, index) {
+		switch (mood) {
+			case 'bad':
+				data.textContent = moodArray[index].emotions.bad;
+				break;
+			case 'bored':
+				data.textContent = moodArray[index].emotions.bored;
+				break;
+			case 'happy':
+				data.textContent = moodArray[index].emotions.happy;
+				break;
+			default:
+				break;
+		}
+	}
+
+	getSumMoodStatisticsAllDays(moodArray) {
+		const bad = [];
+		const bored = [];
+		const happy = [];
+		moodArray.forEach(el => {
+			bad.push(el.emotions.bad);
+			bored.push(el.emotions.bored);
+			happy.push(el.emotions.happy);
+		});
+
+		return {
+			bad: this.sum(bad),
+			bored: this.sum(bored),
+			happy: this.sum(happy),
+		};
+	}
+	sum(array) {
+		return array.reduce((acc, curr) => acc + curr, 0);
+	}
+
+	showMoodStatisticsAllDays(mood, data, sumMood) {
+		switch (mood) {
+			case 'bad':
+				data.textContent = sumMood.bad;
+				break;
+			case 'bored':
+				data.textContent = sumMood.bored;
+				break;
+			case 'happy':
+				data.textContent = sumMood.happy;
+				break;
+			default:
+				break;
 		}
 	}
 }
