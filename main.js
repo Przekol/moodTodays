cef5261d240d904dd4ea4c241468bcc31c1645a1
Przemyslaw <przemyslaw.r.wojciechowski@gmail.com>
2022-04-21 13:19:37 +0200

    removed unnecessary code

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-import {LocalStorage} from './js/LocalStorage.js';
-import {Theme} from './js/Theme.js';
-import {Popup} from './js/Popup.js';
-import {Form} from './js/Form.js';
-import {Mood} from './js/Mood.js';
-import {RippleEffect} from './js/RippleEffect.js';
+import { LocalStorage } from './js/LocalStorage.js';
+import { Theme } from './js/Theme.js';
+import { Popup } from './js/Popup.js';
+import { Form } from './js/Form.js';
+import { RippleEffect } from './js/RippleEffect.js';
 
 const localStorage = new LocalStorage();
 const theme = new Theme();
@@ -14,8 +13,8 @@ rippleEffect.init();
 theme.init();
 
 if (!localStorage.getUserName()) {
-    popup.init();
-    formPopup.init();
+	popup.init();
+	formPopup.init();
 } else {
-    popup.moodInit();
+	popup.moodInit();
 }
